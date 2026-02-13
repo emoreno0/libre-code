b6c672707b550d28168f8e4b7cd7b3f5bc79728e
emoreno0 <ericmorenowork@gmail.com>
2026-02-13 10:02:29 +0100

    Add: Count lines

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -3,22 +3,35 @@ import { StateType } from "../../state/State"
 
 export default function CodeEditor() {
   const [currentState, setCurrentState] = useState<StateType | null>()
+  const [lines, setLines] = useState(Number)
 
   useEffect(() => {
     window.electronAPI.onStateChanged((state) => {
       setCurrentState(state)
     })
+    setLines(currentState?.content ? currentState.content.split("\n").length : 0)
   })
 
   return (
     <div className="h-full min-h-screen w-full">
       {
         currentState ?
-          <pre>
-            <code>
-              {currentState.content}
-            </code>
-          </pre>
+          <div className="flex my-1.5">
+            <div className="mx-2">
+              {
+                Array.from({ length: lines }, (_, index) => (
+                  <p>
+                    {index + 1}
+                  </p>
+                ))
+              }
+            </div>
+            <pre>
+              <code>
+                {currentState.content}
+              </code>
+            </pre>
+          </div>
           : <></>
       }
     </div>
